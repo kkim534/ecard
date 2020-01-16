d27381e6a666c0c7df600282e41e95525e18d96c
Alex Kim <kkim534@aucklanduni.ac.nz>
2020-01-16 20:00:02 +1300

    Implemented DatePicker component

--- src/pages/home-page/Home.tsx
+++ src/pages/home-page/Home.tsx
@@ -1,29 +1,42 @@
 import 'react-dates/initialize';
 import React, { useState } from 'react';
 import './NavMenu.css';
-// import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import { DateRangePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
+import moment from 'moment';
 
-export default React.PureComponent;
-export const pureComponentAvailable = true;
+var constant = require('react-dates/constants')
 
-export const Home: React.FunctionComponent = (props: any) => { 
-  // static displayName = Home.name;
+export interface DateState {
+  startDate: moment.Moment | null;
+  endDate: moment.Moment | null;
+}
+
+export const Home: React.FunctionComponent = (props: any) => {
+
+  let [date, setDate] = useState<DateState>({ startDate: moment(),
+                                  endDate: null
+                                  });                                                                    
+  let [focusedInput, setFocusedInput] = useState(constant.START_DATE);
 
   return (
     <div>
       <h1>Events</h1>
-          <p>This portal is used for sending greeting messages to ASB</p>
-      {/* <DateRangePicker
-        startDate={null} // momentPropTypes.momentObj or null,
-        startDateId="start_date_id" // PropTypes.string.isRequired,
-        endDate={null} // momentPropTypes.momentObj or null,
-        endDateId="end_date_id" // PropTypes.string.isRequired,
-        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-        focusedInput={null} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-        // onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-      /> */}
+      <p>This portal is used for sending greeting messages to ASB</p>
+      <DateRangePicker
+        startDate={date.startDate}
+        // startDate={null}
+        startDateId="startDate" // PropTypes.string.isRequired,
+        endDate={date.endDate}
+        // endDate={null}
+        endDateId="endDate" // PropTypes.string.isRequired,
+        focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+        onDatesChange={({ startDate, endDate }) => {setDate({ startDate: startDate,
+                                                            endDate: endDate})}}
+        onFocusChange={focusedInput => {setFocusedInput(focusedInput || constant.END_DATE )}} 
+      />
     </div>
   );
-  
 }
+
+
