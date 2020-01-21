3b51d22ecc85c1e014eba072582e5a6aa4852467
Alex Kim <kkim534@aucklanduni.ac.nz>
2020-01-21 16:06:32 +1300

    Image upload API call fleshed (not tested)

--- src/pages/home-page/Home.tsx
+++ src/pages/home-page/Home.tsx
@@ -2,9 +2,10 @@ import 'react-dates/initialize';
 import './NavMenu.css';
 import React, { useState } from 'react';
 import { DateRangePicker, SingleDatePicker } from 'react-dates';
-import { Form, Button, Col } from 'react-bootstrap';
+import { Form, Button, Col, Row } from 'react-bootstrap';
 import 'react-dates/lib/css/_datepicker.css';
 import moment from 'moment';
+import axios from 'axios';
 import './Home.css'
 
 var constant = require('react-dates/constants')
@@ -19,6 +20,28 @@ export interface sendDateState {
 
 export const Home: React.FunctionComponent = (props: any) => {
 
+  const handleChange = (e: any) => {
+    console.log(e.target.files)
+    e.preventDefault(); //prevent browser refresh
+    var apiBaseUrl = axios.defaults.baseURL + "user/upload";
+    if (e.target.files.length > 0) {
+      var filesArray = e.target.files;
+      let f = new FormData();
+      for (var i in filesArray) {
+        //console.log("files",filesArray[i][0]);
+        f = new FormData();
+        f.append("File", filesArray[i][0])
+        axios.post(apiBaseUrl, f, {
+          headers: { 'Content-Type': 'multipart/form-data' }
+        });
+      }
+      alert("File upload completed");
+    }
+    else {
+      alert("Please select files first");
+    }
+  }
+  
   // DateRangePicker
   let [focusedInput, setFocusedInput] = useState(null);
   let [date, setDate] = useState<DateState>({
@@ -32,69 +55,82 @@ export const Home: React.FunctionComponent = (props: any) => {
 
   return (
     <>
-      <div>
+      <h1>Create events</h1>
 
-        <h1>Create events</h1>
+      <div className='form-container container'>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <Form>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formEventName">
+                  <Form.Label>Event name</Form.Label>
+                  <Form.Control type="textarea" placeholder="Enter event name" />
+                </Form.Group>
 
-        <div className='form-container'>
-          <Form>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formEventName">
-                <Form.Label>Event name</Form.Label>
-                <Form.Control type="textarea" placeholder="Enter event name" />
+                <Form.Group as={Col} controlId="formCorporateMessage">
+                  <Form.Label>Datacom message</Form.Label>
+                  <Form.Control type="textarea" placeholder="Enter corporate message" />
+                </Form.Group>
+              </Form.Row>
+
+              <Form.Group controlId="formEventDetails">
+                <Form.Label>Event details</Form.Label>
+                <Form.Control as="textarea" rows="3" placeholder="Enter event details" />
               </Form.Group>
 
-              <Form.Group as={Col} controlId="formCorporateMessage">
-                <Form.Label>Datacom message</Form.Label>
-                <Form.Control type="textarea" placeholder="Enter corporate message" />
-              </Form.Group>
-            </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} controlId="organisationSelect">
+                  <Form.Label>Select organisation</Form.Label>
+                  <Form.Control as="select">
+                    <option>Datacom</option>
+                    <option>ASB</option>
+                  </Form.Control>
+                </Form.Group>
+                <Form.Group as={Col} controlId="imageInput">
+                  <Form.Label>Upload corporation image</Form.Label>
+                  <Form.Control type="file" accept="image/*" onChange={handleChange}>
+                  </Form.Control>
+                </Form.Group>
+              </Form.Row>
 
-            <Form.Group controlId="formEventDetails">
-              <Form.Label>Datacom message</Form.Label>
-              <Form.Control as = "textarea" rows = "3" placeholder="Enter event details" />
-            </Form.Group>
 
-            <Form.Group controlId="organisationSelect">
-              <Form.Label>Select organisation</Form.Label>
-              <Form.Control as="select">
-                <option>Datacom</option>
-                <option>ASB</option>
-              </Form.Control>
-            </Form.Group>
 
-            <Button variant="primary" type="submit">
-              Submit
+              <div className='date-container'>
+                <DateRangePicker
+                  startDate={date.startDate}
+                  startDateId="startDate"
+                  endDate={date.endDate}
+                  endDateId="endDate"
+                  focusedInput={focusedInput}
+                  onFocusChange={focusedInput => setFocusedInput(focusedInput || constant.END_DATE)}
+                  onDatesChange={({ startDate, endDate }) => setDate({
+                    startDate: startDate,
+                    endDate: endDate
+                  })}
+                  onClose={focusedInput => setFocusedInput(null)}
+                />
+
+                <SingleDatePicker
+                  date={sendDateState.sendDate} // momentPropTypes.momentObj or null
+                  onDateChange={(date: any) => setSendDate({ sendDate: date })} // PropTypes.func.isRequired
+                  focused={focus} // PropTypes.bool
+                  onFocusChange={(focused: any) => setFocus(focused)} // PropTypes.func.isRequired
+                  id="sendDate" // PropTypes.string.isRequired,
+                  onClose={focused => setFocus(false)}
+                />
+              </div>
+              <Row>
+                <Col>
+                  <br />
+                  <Button variant="primary" type="submit">
+                    Submit
           </Button>
-          </Form>
-        </div>
-        <div className='date-container'>
-          <DateRangePicker
-            startDate={date.startDate}
-            startDateId="startDate"
-            endDate={date.endDate}
-            endDateId="endDate"
-            focusedInput={focusedInput}
-            onFocusChange={focusedInput => setFocusedInput(focusedInput || constant.END_DATE)}
-            onDatesChange={({ startDate, endDate }) => setDate({
-              startDate: startDate,
-              endDate: endDate
-            })}
-            onClose={focusedInput => setFocusedInput(null)}
-          />
-
-          <SingleDatePicker
-            date={sendDateState.sendDate} // momentPropTypes.momentObj or null
-            onDateChange={(date: any) => setSendDate({ sendDate: date })} // PropTypes.func.isRequired
-            focused={focus} // PropTypes.bool
-            onFocusChange={(focused: any) => setFocus(focused)} // PropTypes.func.isRequired
-            id="sendDate" // PropTypes.string.isRequired,
-            onClose={focused => setFocus(false)}
-          />
-        </div>
-
+                </Col>
+              </Row>
+            </Form>
+          </Col>
+        </Row>
       </div>
-
     </>
   );
 }
--- src/pages/home-page/NavMenu.tsx
+++ src/pages/home-page/NavMenu.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
@@ -15,7 +15,6 @@ export const  NavMenu : React.FunctionComponent = (props: any) => {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
             <NavbarBrand tag={Link} to="/">ECard</NavbarBrand>
             <NavbarToggler onClick={() => {setToggleState({collapsed: !toggleState})}} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!toggleState} navbar>
@@ -31,7 +30,6 @@ export const  NavMenu : React.FunctionComponent = (props: any) => {
                 </NavItem>
               </ul>
             </Collapse>
-          </Container>
         </Navbar>
       </header>
     );
